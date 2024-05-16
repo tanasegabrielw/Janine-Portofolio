98aba43b80f4e24226f34f907c4da039e4258fcb
Gabriel T <72333512+tanasegabrielw@users.noreply.github.com>
2024-05-16 06:50:00 +0300

    Move js to external script file

---
+++ code.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', async () => {
+    const response = await fetch('en/galery.html');
+    const galleryHTML = await response.text();
+    const galleryDoc = new DOMParser().parseFromString(galleryHTML, 'text/html');
+    const imageContainers = galleryDoc.querySelectorAll('.image-container');
+    
+    // Randomly select an index
+    const randomIndex = Math.floor(Math.random() * imageContainers.length);
+    
+    // Get the randomly selected image container
+    const randomImageContainer = imageContainers[randomIndex];
+
+    // Clone the selected image container
+    const clonedContainer = randomImageContainer.cloneNode(true);
+
+    // Append the cloned container to the index.html page
+    document.getElementById('galleryImages').appendChild(clonedContainer);
+});
