b76ad3bb1b7f5828f7cf2f609c9123c5933f0dbc
Gabriel T <72333512+tanasegabrielw@users.noreply.github.com>
2024-05-17 10:07:01 +0300

    Add content to galery

--- code.js
+++ code.js
@@ -1,19 +1,44 @@
-    window.addEventListener('DOMContentLoaded', async () => {
-        const response = await fetch('galery.html');
-        const galleryHTML = await response.text();
-        const galleryDoc = new DOMParser().parseFromString(galleryHTML, 'text/html');
-        const imageContainers = galleryDoc.querySelectorAll('.image-container');
-    
-        // Randomly select an index
-        const randomIndex = Math.floor(Math.random() * imageContainers.length);
-    
-        // Get the randomly selected image container
-        const randomImageContainer = imageContainers[randomIndex];
-    
-        // Clone the selected image container
-        const clonedContainer = randomImageContainer.cloneNode(true);
-    
-        // Append the cloned container to the index.html page
-        document.getElementById('galleryImages').appendChild(clonedContainer);
+window.addEventListener('DOMContentLoaded', async () => {
+    const response = await fetch('galery.html');
+    const galleryHTML = await response.text();
+    const galleryDoc = new DOMParser().parseFromString(galleryHTML, 'text/html');
+    const imageContainers = galleryDoc.querySelectorAll('.image-container');
+
+    const randomIndex = Math.floor(Math.random() * imageContainers.length);
+    const randomImageContainer = imageContainers[randomIndex];
+
+    const clonedContainer = randomImageContainer.cloneNode(true);
+    document.getElementById('galleryImages').appendChild(clonedContainer);
+
+    // Call the function to add event listeners after appending the container
+    addEventListeners();
+});
+
+function addEventListeners() {
+    const containers = document.querySelectorAll('.image-caption-box');
+    const imageContainers = document.querySelectorAll('.image-container');
+
+    containers.forEach(container => {
+        const image = container.querySelector('img');
+        const mobileCaption = container.querySelector('.mobile-caption');
+        const caption = container.querySelector('.caption');
+        // For Mobile always keeps caption ON //
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            mobileCaption.style.opacity = '1';
+        } else {
+            container.addEventListener('mouseenter', () => {
+                caption.style.opacity = '1';
+            });
+            container.addEventListener('mouseleave', () => {
+                caption.style.opacity = '0';
+            });
+        }
     });
 
+    imageContainers.forEach(imageContainer => {
+        const imageTitle = imageContainer.querySelector('.title');
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            imageTitle.style.display = 'none';
+        }
+    });
+}
