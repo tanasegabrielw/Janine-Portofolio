d17ea5aac601f453f7d5ecc7b6fba012e3929762
Gabriel T <72333512+tanasegabrielw@users.noreply.github.com>
2024-05-20 08:35:03 +0300

    Make De version of website

--- code_de.js
+++ code_de.js
@@ -1,9 +1,10 @@
 window.addEventListener('DOMContentLoaded', async () => {
-        const response = await fetch('galery.html');
+        const response = await fetch('galery_de.html');
         const galleryHTML = await response.text();
         const galleryDoc = new DOMParser().parseFromString(galleryHTML, 'text/html');
         const imageContainers = galleryDoc.querySelectorAll('.image-container');
         
+        // Randomly select an index
         const randomIndex = Math.floor(Math.random() * imageContainers.length);
         const randomImageContainer = imageContainers[randomIndex];
         
@@ -11,5 +12,5 @@ window.addEventListener('DOMContentLoaded', async () => {
         const clonedContainer = randomImageContainer.cloneNode(true);
         
         // Append the cloned container to the index.html page
-        document.getElementById('galleryImages').appendChild(clonedContainer);   
+        document.getElementById('galleryImages').appendChild(clonedContainer);
 });
